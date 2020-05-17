df445ea7042421f9206eb02573ac529336b779d1
yerkebulan19971212 <yerkebulan19971212@gmail.com>
2020-05-18 03:52:40 +0600

    category model

--- cores/models.py
+++ cores/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=128)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='children',
+        blank=True,
+        null=True
+    )
+    slug = models.SlugField(unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        full_name_of_category = [self.title]
+
+        parent = self.parent
+
+        while parent:
+            full_name_of_category.append(parent.title)
+            parent = parent.parent
